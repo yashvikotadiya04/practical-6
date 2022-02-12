2317ba000259a1680dc908482fc9179e7d211f6d
yashvikotadiya04 <96778187+yashvikotadiya04@users.noreply.github.com>
2022-02-12 19:09:42 +0530

    Add files via upload

---
+++ prac_6.py
@@ -0,0 +1,9 @@
+from collections import Counter
+t=int(input ())        #number of test cases
+l=list()               #list for storing string
+for _ in range (t):
+    l.append(input())  #add a string to the existing list
+x=Counter(l)           #counter() count the  items in an iterable list
+
+print (len(x))         #length of counter function
+print(*x.values())      #unpackaging the item using asterisk operator
